951a75d4535ae21c56910d68b8546acbc8fd1a4e
victoriamartinez23 <victoriamartinezruiz23@gmail.com>
2024-07-01 18:59:03 +0000

    Generador de excusas listo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function obtenerElementoAleatorio(array) {
+    let indiceAleatorio = Math.floor(Math.random() * array.length);
+    return array[indiceAleatorio];
+  }
+
+  function excusaRandom() {
+    let randomWho = obtenerElementoAleatorio(who);
+    let randomAction = obtenerElementoAleatorio(action);
+    let randomWhat = obtenerElementoAleatorio(what);
+    let randomWhen = obtenerElementoAleatorio(when);
+    return randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen;
+  }
+
+  let elemento = document.getElementById("excusa");
+  let randomExcusa = excusaRandom();
+  elemento.innerHTML = randomExcusa;
 };
